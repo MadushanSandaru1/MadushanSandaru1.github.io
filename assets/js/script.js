8eaa89afa609f2ab58a94079d179f56ca5118721
Madushan Sandaruwan <madushansandaru1@gmail.com>
2024-12-27 15:56:48 +0530

    Add images

--- assets/js/script.js
+++ assets/js/script.js
@@ -342,5 +342,24 @@
         return false;
     });
 
+    /* ============================================================ */
+    /* Age Calculate
+    /* ============================================================ */
+    var dob = new Date("01/17/1998");
+    //calculate month difference from current date in time
+    var month_diff = Date.now() - dob.getTime();
+
+    //convert the calculated difference in date format
+    var age_dt = new Date(month_diff);
+
+    //extract year from date
+    var year = age_dt.getUTCFullYear();
+
+    //calculate the age of the user
+    var age = Math.abs(year - 1970);
+
+    //display the calculated age
+    document.getElementById("age-display").innerHTML =  age + " Years";
+
 })(jQuery);
 // jQuery Ended
